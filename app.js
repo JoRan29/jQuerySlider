4117abf375168c70906b7b2e4275aba5bcd85474
JoRan29 <jokavana29@gmail.com>
2020-05-01 19:39:58 +0200

    jQuery selectors

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 $(document).ready(() => {
   $(".myLink").on("click", () => {
-    alert("Clicked!");
+    //     $("h1").addClass("red");
+    //     $("h2").fadeOut(500);
+    //     $("h1").slideToggle(500);
   });
-  $("h1").addClass("red");
+  $(".myLink").click(() => {
+    $("h1").text("Hello!");
+    $("h3").html("<h2> What? </h2>");
+    $("h1").css("font-size", "140px");
+  });
 });
